90532be0440a5be0c86b24b9b40277edc7d040ca
DukHyung Kim <idealismboy@gmail.com>
2023-11-15 23:54:49 +0900

    add animation

--- lib/yummy_chat/login_signup_screen.dart
+++ lib/yummy_chat/login_signup_screen.dart
@@ -23,21 +23,21 @@ class _LoginSignupScreenState extends State<LoginSignupScreen> {
             left: 0,
             child: Container(
               height: 300,
-              decoration: BoxDecoration(
+              decoration: const BoxDecoration(
                 image: DecorationImage(
                   image: AssetImage('assets/red.jpg'),
                   fit: BoxFit.fill,
                 ),
               ),
               child: Container(
-                padding: EdgeInsets.only(top: 90, left: 20),
+                padding: const EdgeInsets.only(top: 90, left: 20),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     RichText(
                       text: TextSpan(
                         text: "Welcome",
-                        style: TextStyle(
+                        style: const TextStyle(
                           letterSpacing: 1.0,
                           fontSize: 25,
                           color: Colors.white,
@@ -45,7 +45,7 @@ class _LoginSignupScreenState extends State<LoginSignupScreen> {
                         children: [
                           TextSpan(
                             text: isSignupScreen ? " to Yummy chat!" : " back",
-                            style: TextStyle(
+                            style: const TextStyle(
                               letterSpacing: 1.0,
                               fontSize: 25,
                               color: Colors.white,
@@ -55,14 +55,14 @@ class _LoginSignupScreenState extends State<LoginSignupScreen> {
                         ],
                       ),
                     ),
-                    SizedBox(
+                    const SizedBox(
                       height: 5,
                     ),
                     Text(
                       isSignupScreen
                           ? "Signup or continue"
                           : "Signin to continue",
-                      style: TextStyle(
+                      style: const TextStyle(
                         letterSpacing: 1.0,
                         color: Colors.white,
                       ),
@@ -72,13 +72,17 @@ class _LoginSignupScreenState extends State<LoginSignupScreen> {
               ),
             ),
           ),
-          Positioned(
+          AnimatedPositioned(
+            duration: const Duration(milliseconds: 500),
+            curve: Curves.easeIn,
             top: 180,
-            child: Container(
-              padding: EdgeInsets.all(20.0),
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 500),
+              curve: Curves.easeIn,
+              padding: const EdgeInsets.all(20.0),
               height: isSignupScreen ? 280.0 : 250.0,
               width: MediaQuery.of(context).size.width - 40,
-              margin: EdgeInsets.symmetric(horizontal: 20.0),
+              margin: const EdgeInsets.symmetric(horizontal: 20.0),
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(15.0),
@@ -117,7 +121,7 @@ class _LoginSignupScreenState extends State<LoginSignupScreen> {
                             ),
                             if (!isSignupScreen)
                               Container(
-                                margin: EdgeInsets.only(top: 3),
+                                margin: const EdgeInsets.only(top: 3),
                                 height: 2,
                                 width: 55,
                                 color: Colors.orange,
@@ -147,7 +151,7 @@ class _LoginSignupScreenState extends State<LoginSignupScreen> {
                             ),
                             if (isSignupScreen)
                               Container(
-                                margin: EdgeInsets.only(top: 3),
+                                margin: const EdgeInsets.only(top: 3),
                                 height: 2,
                                 width: 55,
                                 color: Colors.orange,
@@ -159,12 +163,12 @@ class _LoginSignupScreenState extends State<LoginSignupScreen> {
                   ),
                   if (isSignupScreen)
                     Container(
-                      margin: EdgeInsets.only(top: 20),
+                      margin: const EdgeInsets.only(top: 20),
                       child: Form(
                         child: Column(
                           children: [
                             TextFormField(
-                              decoration: InputDecoration(
+                              decoration: const InputDecoration(
                                 prefixIcon: Icon(
                                   Icons.account_circle,
                                   color: Palette.iconColor,
@@ -193,11 +197,11 @@ class _LoginSignupScreenState extends State<LoginSignupScreen> {
                                 contentPadding: EdgeInsets.all(10),
                               ),
                             ),
-                            SizedBox(
+                            const SizedBox(
                               height: 8,
                             ),
                             TextFormField(
-                              decoration: InputDecoration(
+                              decoration: const InputDecoration(
                                 prefixIcon: Icon(
                                   Icons.email,
                                   color: Palette.iconColor,
@@ -226,11 +230,11 @@ class _LoginSignupScreenState extends State<LoginSignupScreen> {
                                 contentPadding: EdgeInsets.all(10),
                               ),
                             ),
-                            SizedBox(
+                            const SizedBox(
                               height: 8,
                             ),
                             TextFormField(
-                              decoration: InputDecoration(
+                              decoration: const InputDecoration(
                                 prefixIcon: Icon(
                                   Icons.lock,
                                   color: Palette.iconColor,
@@ -265,12 +269,12 @@ class _LoginSignupScreenState extends State<LoginSignupScreen> {
                     ),
                   if (!isSignupScreen)
                     Container(
-                      margin: EdgeInsets.only(top: 20),
+                      margin: const EdgeInsets.only(top: 20),
                       child: Form(
                         child: Column(
                           children: [
                             TextFormField(
-                              decoration: InputDecoration(
+                              decoration: const InputDecoration(
                                 prefixIcon: Icon(
                                   Icons.account_circle,
                                   color: Palette.iconColor,
@@ -299,11 +303,11 @@ class _LoginSignupScreenState extends State<LoginSignupScreen> {
                                 contentPadding: EdgeInsets.all(10),
                               ),
                             ),
-                            SizedBox(
+                            const SizedBox(
                               height: 8,
                             ),
                             TextFormField(
-                              decoration: InputDecoration(
+                              decoration: const InputDecoration(
                                 prefixIcon: Icon(
                                   Icons.lock,
                                   color: Palette.iconColor,
@@ -340,14 +344,16 @@ class _LoginSignupScreenState extends State<LoginSignupScreen> {
               ),
             ),
           ),
-          Positioned(
-            top: isSignupScreen ? 430 : 390,
+          AnimatedPositioned(
+            duration: const Duration(milliseconds: 500),
+            curve: Curves.easeIn,
+            top: isSignupScreen ? 420 : 390,
             right: 0,
             left: 0,
             child: Center(
               child: Container(
                 child: Container(
-                  padding: EdgeInsets.all(15),
+                  padding: const EdgeInsets.all(15),
                   height: 90,
                   width: 90,
                   decoration: BoxDecoration(
@@ -356,7 +362,7 @@ class _LoginSignupScreenState extends State<LoginSignupScreen> {
                   ),
                   child: Container(
                     decoration: BoxDecoration(
-                      gradient: LinearGradient(
+                      gradient: const LinearGradient(
                         colors: [
                           Colors.orange,
                           Colors.red,
@@ -370,11 +376,11 @@ class _LoginSignupScreenState extends State<LoginSignupScreen> {
                           color: Colors.black.withOpacity(0.3),
                           spreadRadius: 1,
                           blurRadius: 1,
-                          offset: Offset(0, 1),
+                          offset: const Offset(0, 1),
                         )
                       ],
                     ),
-                    child: Icon(
+                    child: const Icon(
                       Icons.arrow_forward,
                       color: Colors.white,
                     ),
@@ -390,22 +396,22 @@ class _LoginSignupScreenState extends State<LoginSignupScreen> {
             child: Column(
               children: [
                 Text(isSignupScreen ? "or Signup with" : "or Signin with"),
-                SizedBox(
+                const SizedBox(
                   height: 10,
                 ),
                 TextButton.icon(
                   onPressed: () {},
                   style: TextButton.styleFrom(
-                    primary: Colors.white,
-                    minimumSize: Size(155, 40),
+                    foregroundColor: Colors.white,
+                    minimumSize: const Size(155, 40),
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(20),
                     ),
                     backgroundColor: Palette.googleColor,
                   ),
-                  icon: Icon(Icons.add),
-                  label: Text("Google"),
-                )
+                  icon: const Icon(Icons.add),
+                  label: const Text("Google"),
+                ),
               ],
             ),
           )
